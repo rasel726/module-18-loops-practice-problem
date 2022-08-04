f67975fc62b5a4d385c03266e3b33785bda8cafe
rasel726 <md.rasel2726@gmail.com>
2022-08-05 01:54:31 +0600

    revision file added

---
+++ revision.js
@@ -0,0 +1,24 @@
+// string type variable
+var bottle = 'water';
+var bottleColor = 'yello';
+var penColor = 'blue';
+// number type variable
+var sunGlassPrice = 120;
+// boolean type variable
+var isExpensive = true;
+
+var tableItems = ['bottle', 'pen', 'mouse', 'sunglass', 'paper'];
+
+var item3 = tableItems[3];
+tableItems[4] = 'notebook';
+var penIndex = tableItems.indexOf('pen');
+// push pop
+
+// conditionals 
+
+if(tableItems.length > 4){
+
+}
+else{
+    
+}
