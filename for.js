13c421f3b06e1477c70cc249f8492001689141aa
rasel726 <md.rasel2726@gmail.com>
2022-08-06 00:56:14 +0600

    for file added

---
+++ for.js
@@ -0,0 +1,31 @@
+// var rostGiven = 0;
+// while(rostGiven < 7){
+//     console.log('Rost Den, Please !!');
+//     rostGiven++;
+// }
+
+// for loop
+// for(var rostGiven = 0; rostGiven < 7; rostGiven++){
+//     console.log('Rost Den, Please !!');
+// }
+
+// simple version of for loop
+// console.log('Simple For Loop')
+// for(var i = 0; i <= 7; i++){
+//     console.log(i);
+// }
+
+// jodi amra 1 to 10 porjonto ber korte chai
+// for(var i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+// jodi 1 to 20 er moddhe jora number ber korte chai
+// for(var i = 0; i <= 20; i += 2){
+//     console.log(i);
+// }
+
+// jodi ekoi vabe bijor number gulo dekhte chai
+for(var i = 1; i <= 20; i += 2){
+    console.log(i);
+}
