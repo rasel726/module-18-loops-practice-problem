730f6b3b170b26ced682e1a2dfb1aad40651fea9
rasel726 <md.rasel2726@gmail.com>
2022-08-06 02:39:01 +0600

    avarage file added

---
+++ avarage.js
@@ -0,0 +1,12 @@
+// practice problem 2
+var mathMarks = 75.25;
+var biologyMarks = 65;
+var chemistryMarks = 80;
+var physicsMarks = 35.45;
+var banglaMarks = 99.50;
+
+var totalMarks = mathMarks + biologyMarks + chemistryMarks + physicsMarks + banglaMarks;
+console.log(totalMarks);
+var avarage = totalMarks / 5;
+var avarageTwoDecimal = avarage.toFixed(2);
+console.log(avarageTwoDecimal);
