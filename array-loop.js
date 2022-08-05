f3a54fb1646e2283a76a27730bda997ca46100ec
rasel726 <md.rasel2726@gmail.com>
2022-08-06 01:23:42 +0600

    array-loop.js

---
+++ array-loop.js
@@ -0,0 +1,16 @@
+// for(var i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+// target: display every elements of an array
+// var numbers =[45, 87, 89, 56, 32, 51, 25, 30, 50];
+// for(var i = 0; i < numbers.length; i++){
+//     var number = numbers[i];
+//     console.log(number);
+// }
+
+var items =['bottle', 'mouse', 'sunglass', 'pen', 'speaker', 'khata', 'kolom'];
+for(var i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
