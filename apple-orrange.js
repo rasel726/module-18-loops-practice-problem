8862d071a9c8c6c326b3bdf9070c162f4784f0f8
rasel726 <md.rasel2726@gmail.com>
2022-08-06 02:30:34 +0600

    apple-orrange file added

---
+++ apple-orrange.js
@@ -0,0 +1,10 @@
+// practice problem 1
+var moneyGiven = 1000;
+var applePrice = 400;
+var orrangePrice = 300;
+
+var totalPrice = applePrice + orrangePrice;
+console.log(totalPrice);
+var getBack = moneyGiven - totalPrice;
+console.log(getBack);
+
