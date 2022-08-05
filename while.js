3a7baf84a253bea1d1882a046867eff0953659ec
rasel726 <md.rasel2726@gmail.com>
2022-08-06 00:01:05 +0600

    while file added

---
+++ while.js
@@ -0,0 +1,21 @@
+/**
+ * 1. Loop variable
+ * 2. condition inside while
+ * 3. Loop body
+ * 4. Change the loop varible
+ */
+
+// if(condition){
+
+// }
+
+    // rostGiven = rostGiven + 1;
+    // rostGiven += 1;
+
+
+var rostGiven = 0;
+while(rostGiven < 7) {
+    console.log('Rost den, Please !!')
+    console.log(rostGiven);
+    rostGiven++;
+}
