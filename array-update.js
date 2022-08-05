6fc43c585fd59c0d2d2e3129412668ea6b4f4975
rasel726 <md.rasel2726@gmail.com>
2022-08-06 02:59:15 +0600

    array-update file added

---
+++ array-update.js
@@ -0,0 +1,9 @@
+// practice problem 1
+var fruits =['Apple', 'Banana', 'Orrange'];
+var bananaIndex = fruits.indexOf('Banana');
+fruits[bananaIndex] = 'Mango';
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+fruits.push('Watermelon');
+console.log(fruits);
