398aa512780cc59c8842e1b308218e99dbc67900
rasel726 <md.rasel2726@gmail.com>
2022-08-06 02:08:57 +0600

    continue file added

--- break.js
+++ break.js
@@ -23,7 +23,6 @@
 //     console.log(item);
 // }
 
-// target: display every elements of an array
 var numbers =[45, 87, 89, 56, 32, 51, 25, 30, 50, 188];
 for(var i = 0; i < numbers.length; i++){
     var number = numbers[i];
