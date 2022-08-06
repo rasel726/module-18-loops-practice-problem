123b5812c8ee5b70cd1bcf4672584387d8488742
rasel726 <md.rasel2726@gmail.com>
2022-08-06 12:49:46 +0600

    summary file added

---
+++ summary.js
@@ -0,0 +1,20 @@
+// basic while loop
+// var num = 0;
+// while(num <= 10){
+//     console.log(num);
+//     num++;
+// }
+
+// basic for loop
+// for(var i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// break kivabe kaj kore
+for(var i = 1; i <= 10; i++){
+    console.log(i);
+    if(i == 5){    // 1-5 porjonto count korar por berak korbe
+        break;
+    }
+}
+
